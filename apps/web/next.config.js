01eae6772e70513e3d64458d27aaa78a5a9e1604
Vivek Jilla <vivekjilla7@gmail.com>
2023-03-20 22:46:11 +0530

    update

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,5 +1,16 @@
+const path = require('path');
+
 module.exports = {
   reactStrictMode: true,
   transpilePackages: ["ui"],
-  output: "standalone"
+  output: 'standalone',
+  experimental: {
+    // this includes files from the monorepo base two directories up
+    outputFileTracingRoot: path.join(__dirname, '../../'),
+    outputFileTracingIncludes: {
+      '/eslint-config-custom': ['./eslint-config-custom/**/*'],
+      '/tsconfig': ['./tsconfig/**/*'],
+      '/ui': ['./ui/**/*'],
+    }
+  },
 };
